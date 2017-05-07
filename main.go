0cc12f9a00324d035f70ab88f8eca84a66663062
MacTaylor <1159985@gmail.com>
2017-05-07 20:56:43 +0800

    add grom

--- main.go
+++ main.go
@@ -7,7 +7,20 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/jinzhu/gorm"
+
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+
+	"time"
 )
+
+type GPS_DATA struct {
+	gorm.Model
+	GPS       string
+	TIME      int64
+	DEVICE_ID int
+}
 
 var beep_state = false
 
@@ -19,6 +32,15 @@ func hello(res http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+
+	db, err := gorm.Open("mysql", "root:rootroot@/OLDMAN?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	// Migrate the schema
+	db.AutoMigrate(&GPS_DATA{})
 
 	// 建立 Gin 引擎。
 	router := gin.Default()
@@ -36,6 +58,22 @@ func main() {
 	router.GET("/questBeep", func(c *gin.Context) {
 		beep_state = true
 		c.String(200, "setBeepRequestPendding")
+	})
+
+	//The line after this comment is gps samulation feature.
+	router.GET("/addDevice")
+
+	router.GET("/addGPSData", func(c *gin.Context) {
+		la := c.Param("la")
+		lo := c.Param("lo")
+		device_id := c.Param("device_id")
+
+		deviceIdInteger, _ := strconv.Atoi(device_id)
+
+		fmt.Print(deviceIdInteger)
+		db.Create(&GPS_DATA{GPS: la + lo, TIME: time.Now().Unix(), DEVICE_ID: deviceIdInteger})
+
+		c.String(200, "Received data")
 	})
 
 	// 在 :8080 啟動伺服器。
